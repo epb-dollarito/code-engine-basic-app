abad33c3fe5bad6deebdfaa1ae56da4cf7d3aab5
epb-dollarito <65726591+epb-dollarito@users.noreply.github.com>
2022-08-03 15:47:41 -0500

    Basic App With Docker File to Try Code Engine

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,9 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-CMD [ "python3", "-m" , "flask", "run"]
+USER 1001
+
+EXPOSE 8080
+
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
 
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Response
 
 
 app = Flask(__name__)
@@ -9,8 +10,15 @@ def hello_world():
     return {'msg': 'Hello Docker'}
 
 
+@app.route('/healthz')
+def healthz():
+    resp = Response('ok')
+    resp.headers['Custom-Header'] = 'Awesome'
+    return resp
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='8080')
 
 
 
